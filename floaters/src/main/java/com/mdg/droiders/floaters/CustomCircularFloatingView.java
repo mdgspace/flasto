501b1bbb3b2024d03ecaa9d449609c4857413a38
RohanBh <brohan52@gmail.com>
2017-05-15 15:04:07 +0000

    adds size option for custom floating view

--- floaters/src/main/java/com/mdg/droiders/floaters/CustomCircularFloatingView.java
+++ floaters/src/main/java/com/mdg/droiders/floaters/CustomCircularFloatingView.java
@@ -9,12 +9,19 @@ import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
 import static java.lang.Math.min;
 
-public class CustomCircularFloatingView extends View {
+public class CustomCircularFloatingView extends android.support.v7.widget.AppCompatImageView {
 
-    private int color;
+    public static final int SIZE_NORMAL = 0;
+    public static final int SIZE_SMALL = 1;
+
+    private int colorNormal;
+    private int colorPressed;
+    private int colorRipple;
+    private int floatingViewSize;
     private float floatingViewRadius;
     private Paint paint;
 
@@ -27,29 +34,81 @@ public class CustomCircularFloatingView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        floatingViewRadius = ((float) min(canvas.getWidth(), canvas.getHeight())) / 2;
-        float xcenter = ((float) getWidth()) / 2, ycenter = ((float) getHeight()) / 2;
+        float xcenter = ((float) getCircleSize()) / 2, ycenter = ((float) getCircleSize()) / 2;
         canvas.drawCircle(xcenter,ycenter,floatingViewRadius,paint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int contentWidth = getCircleSize();
+
+        // Resolve the width based on our minimum and the measure spec
+        int minw = contentWidth + getPaddingLeft() + getPaddingRight();
+        int w = resolveSizeAndState(minw, widthMeasureSpec, 0);
+
+        // Ask for a height that would let the view get as big as it can
+        int minh = contentWidth + getPaddingBottom() + getPaddingTop();
+        int h = resolveSizeAndState(minh, heightMeasureSpec, 0);
+
+        // Calling this method determines the measured width and height
+        // Retrieve with getMeasuredWidth or getMeasuredHeight methods later
+        setMeasuredDimension(w, h);
     }
 
     private void setupAttributes(AttributeSet attrs) {
         // Obtain a typed array of attributes
         TypedArray a = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.CustomCircularFloatingView, 0, 0);
+        floatingViewRadius = ((float) getCircleSize())/2;
+
         // Extract custom attributes into member variables
         try {
-            color = a.getColor(R.styleable.CustomCircularFloatingView_color, Color.BLACK);
+            colorNormal = a.getColor(R.styleable.CustomCircularFloatingView_colorNormal, Color.BLACK);
+            colorPressed = a.getColor(R.styleable.CustomCircularFloatingView_colorPressed, Color.BLACK);
+            colorRipple = a.getColor(R.styleable.CustomCircularFloatingView_colorRipple, Color.BLACK);
+            floatingViewSize =a.getInt(R.styleable.CustomCircularFloatingView_floatingViewSize, SIZE_NORMAL);
         } finally {
             // TypedArray objects are shared and must be recycled.
             a.recycle();
         }
     }
 
-    public int getColor() {
-        return color;
+    public int getColorNormal() {
+        return colorNormal;
     }
 
-    public void setColor(int color) {
-        this.color = color;
+    public void setColorNormal(int colorNormal) {
+        this.colorNormal = colorNormal;
+        invalidate();
+        requestLayout();
+    }
+
+    public int getColorPressed() {
+        return colorPressed;
+    }
+
+    public void setColorPressed(int colorPressed) {
+        this.colorPressed = colorPressed;
+        invalidate();
+        requestLayout();
+    }
+
+    public int getColorRipple() {
+        return colorRipple;
+    }
+
+    public void setColorRipple(int colorRipple) {
+        this.colorRipple = colorRipple;
+        invalidate();
+        requestLayout();
+    }
+
+    public int getFloatingViewSize() {
+        return floatingViewSize;
+    }
+
+    public void setFloatingViewSize(int floatingViewSize) {
+        this.floatingViewSize = floatingViewSize;
         invalidate();
         requestLayout();
     }
@@ -57,7 +116,13 @@ public class CustomCircularFloatingView extends View {
     private void setupPaint() {
         paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
-        paint.setColor(color);
+        paint.setColor(colorNormal);
     }
+
+    private int getCircleSize() {
+        return getResources().getDimensionPixelSize(floatingViewSize == SIZE_NORMAL
+                ? R.dimen.size_normal : R.dimen.size_small);
+    }
+
 
 }
